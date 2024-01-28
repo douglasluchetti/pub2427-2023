15af90ed692fcca368d807e4439f22a7bbb220c7
douglasluchetti <132233346+douglasluchetti@users.noreply.github.com>
2024-01-28 20:26:51 -0300

    .

--- index_master.php
+++ index_master.php
@@ -6,7 +6,16 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 
-include("../config.php")
+$host = "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbusername = "g3iwo7l2nhxhu3jj";
+$dbpassword = "l2yt1z9ari3b7ivr";
+$dbname = "e9hxtfiwq6fz1for";
+
+$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 $username = $_SESSION['username'];
 
--- login.php
+++ login.php
@@ -4,7 +4,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    include("config.php")
+    $host = "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbusername = "g3iwo7l2nhxhu3jj";
+    $dbpassword = "l2yt1z9ari3b7ivr";
+    $dbname = "e9hxtfiwq6fz1for";
+
+    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
 
     $query = "SELECT * FROM user WHERE (user_id=? OR email=?) AND password=?";
     $stmt = $conn->prepare($query);

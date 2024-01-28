b3cb5b164160ee55849177f751c9ff5c458ae611
douglasluchetti <132233346+douglasluchetti@users.noreply.github.com>
2024-01-28 19:12:37 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host = "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+$db_user = "g3iwo7l2nhxhu3jj";
+$db_pass = "l2yt1z9ari3b7ivr";
+$db_name = "e9hxtfiwq6fz1for";
+
+$connect = msqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");

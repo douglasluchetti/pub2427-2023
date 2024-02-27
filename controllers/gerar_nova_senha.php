20be56709d9cfb3c590dc8e05ef179cad73d85d3
douglasluchettir <douglas.luchetti@polijunior.com.br>
2024-02-27 17:36:14 -0300

    Update height of input#code

--- controllers/gerar_nova_senha.php
+++ controllers/gerar_nova_senha.php
@@ -1,6 +1,20 @@
 <?php
 
-header("Location: ../views/nova_senha_gerada.php");
+// header("Location: ../views/nova_senha_gerada.php");
+
+include('../config.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+
+    $query = "SELECT * FROM user WHERE (user_id=? OR email=?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("sss", $username, $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    echo $result
+}
 
 exit();
 ?>
--- controllers/login.php
+++ controllers/login.php
@@ -1,19 +1,11 @@
 <?php
+
+include('../config.php');
+
 // Inicia a sessão após a autenticação do usuário bem-sucedida
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
-
-    $host = "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    $dbusername = "g3iwo7l2nhxhu3jj";
-    $dbpassword = "l2yt1z9ari3b7ivr";
-    $dbname = "e9hxtfiwq6fz1for";
-
-    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
 
 
     $query = "SELECT * FROM user WHERE (user_id=? OR email=?) AND password=?";
